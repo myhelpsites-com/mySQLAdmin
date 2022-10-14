340eaac2e2d29f0eec4b72da1b220db3a859f6a4
´myhelpsites-com <gabriel@myhelpsites.com>
2022-10-14 19:28:18 -0300

    feature(MetaHome): create Meta

---
+++ components/meta/Home/MetaHome.tsx
@@ -0,0 +1,32 @@
+export const MetaHome = {
+    fields: [
+        {
+            path: "user",
+            inputType: "text",
+            label: "Usuario:",
+            placeholder: "Digite seu nome de usuario",
+            className: "input-default"
+        },
+        {
+            path: "host",
+            inputType: "text",
+            label: "IP:",
+            placeholder: "Digite seu IP",
+            className: "input-default"
+        },
+        {
+            path: "database",
+            inputType: "text",
+            placeholder: "Digite o nome do seu banco de dados",
+            label: "Banco de dados:",
+            className: "input-default"
+        },
+        {
+            path: "password",
+            inputType: "text",
+            label: "Senha:",
+            placeholder: "Digite sua senha",
+            className: "input-default"
+        }
+    ]
+}
