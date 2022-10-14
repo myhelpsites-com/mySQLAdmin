567b42eeb36ad65f10f9bcbd83a5816168f8f664
´myhelpsites-com <gabriel@myhelpsites.com>
2022-10-14 19:31:58 -0300

    feature(InputGroup): add types and adjust show

--- components/ui/Input/Group/InputGroup.tsx
+++ components/ui/Input/Group/InputGroup.tsx
@@ -1,13 +1,13 @@
 import React from "react";
 import { Inputs } from "../Inputs";
 
-export function InputGroup(props) {
-    const { label, helpText, type } = props.meta;
+export function InputGroup(props): JSX.Element {
+    const { label, helpText, inputType } = props.meta;
     return (
         <>
             <div className="form-group">
-                {label ? <label>{label}</label> : <></>}
-                {Inputs[type]}
+                {label ? <div><label>{label}</label></div> : <></>}
+                {Inputs[inputType](props.meta)}
                 {helpText ? <small id="helpId" className="form-text text-muted">{helpText}</small> : <></>}
             </div>
         </>
